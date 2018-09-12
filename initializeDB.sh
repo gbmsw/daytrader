354d56663bf9b590f0ddcb9b809509009ae5d9d7
jphernandez1587 <33062898+jphernandez1587@users.noreply.github.com>
2018-09-12 16:53:56 -0600

    Modificación para correr background

--- initializeDB.sh
+++ initializeDB.sh
@@ -3,4 +3,4 @@ wget http://localhost:9080/daytrader/config?action=buildDBTables
 /opt/ibm/wlp/bin/server stop defaultServer
 /opt/ibm/wlp/bin/server start defaultServer
 wget http://localhost:9080/daytrader/config?action=buildDB
-/bin/bash
+while true; do sleep 1; done
